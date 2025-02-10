0a2a3dd8bf3027f3e8ece5765eae141be39debf5
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2025-02-10 22:59:43 +0100

    pass status

--- api/routes/books.py
+++ api/routes/books.py
@@ -57,12 +57,12 @@ async def get_book(book_id: int):
         )
     return book
 
-# @router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
-# async def update_book(book_id: int, book: Book) -> Book:
-#     return JSONResponse(
-#         status_code=status.HTTP_200_OK,
-#         content=db.update_book(book_id, book).model_dump(),
-#     )
+@router.put("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def update_book(book_id: int, book: Book) -> Book:
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content=db.update_book(book_id, book).model_dump(),
+    )
 
 
 @router.delete("/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
